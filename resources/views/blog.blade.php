182fe2d5b13cdcf68d555d4797e78ee699182c8b
dicco27 <meutuahdiccolinge@gmail.com>
2024-11-25 07:21:33 +0700

    Mengerjakan Laravel

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,4 @@
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <h3 class="text-xl">Selamat datang di Blog akuh!</h3>
+</x-layout>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,4 @@
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <h3 class="text-xl">Ini Halaman Home Page</h3>
+</x-layout>
